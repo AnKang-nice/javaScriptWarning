74077f8b638d37780d577cd8eaf010b9ac2d3dec
haoankang <2311040049@qq.com>
2025-06-05 15:08:44 +0800

    feat: file

--- proxy defineProperty区别/index.js
+++ proxy defineProperty区别/index.js
@@ -22,7 +22,7 @@ function observer(obj) {
         // 4. 使用 defineProperty 定义属性的 getter 和 setter 方法
         Object.defineProperty(obj, key, {
             get() {
-                // 获取就可以被拦截到
+                // 获取就可以被拦截到   
                 console.log('get', v)
                 return v
             },
@@ -38,17 +38,24 @@ observer(obj)
 observer(arr)
 
 obj.name = 'wangwu'
-console.log(obj.name)
+// console.log(obj.name)
 
 
-obj.sun.name = 'zhaoliu'
-console.log(obj.sun.name)
+function updateName() {
+    
+    document.querySelector('#name').innerHTML = obj.name
+}
 
-obj.sex = '男' // 新增属性   并没有被代理  get  set 方法没有被调用
-console.log(obj.sex)
 
 
-arr[1] = 10
-console.log(arr[1])
-arr[10] = 100 // 数组新增属性   也没有被代理
-console.log(arr[10]) // 100
+// obj.sun.name = 'zhaoliu'
+// console.log(obj.sun.name)
+
+// obj.sex = '男' // 新增属性   并没有被代理  get  set 方法没有被调用
+// console.log(obj.sex)
+
+
+// arr[1] = 10
+// console.log(arr[1])
+// arr[10] = 100 // 数组新增属性   也没有被代理
+// console.log(arr[10]) // 100
